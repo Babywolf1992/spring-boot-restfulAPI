83e5a608566b8a268e74d1a0453de333ff8aa28f
babywolf <591138406@qq.com>
2017-04-12 22:44:46 +0800

    check bug

--- src/main/java/com/babywolf/controller/IndexController.java
+++ src/main/java/com/babywolf/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.babywolf.controller;
 
 import com.babywolf.entity.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,7 @@ public class IndexController {
         return "welcome";
     }
 
+    // @RequestParam 简单类型的绑定，可以出来get和post
     @RequestMapping(value = "/get")
     public HashMap<String, Object> get(@RequestParam String name) {
         HashMap<String, Object> map = new HashMap<String, Object>();
@@ -49,13 +51,12 @@ public class IndexController {
         return map;
     }
 
-
+    // @PathVariable 获得请求url中的动态参数
     @RequestMapping(value = "/get/{id}/{name}")
-    public User getUser(int id, String name) {
+    public User getUser(@PathVariable int id, @PathVariable String name) {
         User user = new User();
         user.setId(id);
         user.setName(name);
         return user;
     }
-
 }
