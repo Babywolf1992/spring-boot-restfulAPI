2315e2aa862ca933ca00503c9b3f2dee07afc8ce
babywolf <591138406@qq.com>
2017-04-12 22:18:53 +0800

    commit

---
+++ src/main/java/com/babywolf/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.babywolf.controller;
+
+/**
+ * Created by babywolf on 17/4/12.
+ */
+public class IndexController {
+}
---
+++ src/main/java/com/babywolf/entity/User.java
@@ -0,0 +1,7 @@
+package com.babywolf.entity;
+
+/**
+ * Created by babywolf on 17/4/12.
+ */
+public class User {
+}
