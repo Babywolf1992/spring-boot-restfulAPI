88c4b94e1ed870a27a6cc3ca3a11febda9d3e800
babywolf <591138406@qq.com>
2017-04-12 22:30:07 +0800

    commit

--- src/main/java/com/babywolf/controller/IndexController.java
+++ src/main/java/com/babywolf/controller/IndexController.java
@@ -1,7 +1,51 @@
 package com.babywolf.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
 /**
  * Created by babywolf on 17/4/12.
  */
+@RestController
+@RequestMapping(value = "/index")
 public class IndexController {
+//    @RequestMapping
+//    public String index() {
+//        return "hello world";
+//    }
+//
+//    // @RequestParam 简单类型的绑定，可以出来get和post
+//    @RequestMapping(value = "/get")
+//    public HashMap<String, Object> get(@RequestParam String name) {
+//        HashMap<String, Object> map = new HashMap<String, Object>();
+//        map.put("title", "hello world");
+//        map.put("name", name);
+//        return map;
+//    }
+//
+//    // @PathVariable 获得请求url中的动态参数
+//    @RequestMapping(value = "/get/{id}/{name}")
+//    public User getUser(@PathVariable int id, @PathVariable String name) {
+//        User user = new User();
+//        user.setId(id);
+//        user.setName(name);
+//        user.setDate(new Date());
+//        return user;
+//    }
+    @RequestMapping
+    public String index() {
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/get")
+    public HashMap<String, Object> get(@RequestParam String name) {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "hello");
+        map.put("name", name);
+        return map;
+    }
+
 }
--- src/main/java/com/babywolf/entity/User.java
+++ src/main/java/com/babywolf/entity/User.java
@@ -4,4 +4,23 @@ package com.babywolf.entity;
  * Created by babywolf on 17/4/12.
  */
 public class User {
+    int id;
+
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
